7d48971d81fd0185092c57c3f734eb1899ac3bbf
Nataly20Nataly <nataliianosova20@gmail.com>
2021-04-06 01:46:37 +0300

    general changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var myCarousel = document.querySelector("#carouselExampleCaptions")
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: false,
+  interval: 1000,
   wrap: false,
   pause: 'hover',
 })
